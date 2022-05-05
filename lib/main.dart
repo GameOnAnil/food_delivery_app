2bde659ca304792286efbe4da073a3e9380f74aa
Anil Thapa <anilthapa.dtechnp@gmail.com>
2022-05-05 16:07:32 +0545

    login changed to token auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,83 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/presentation/screens/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:food_delivery_app/presentation/screens/login_page.dart';
-import 'package:food_delivery_app/presentation/screens/splash_screen.dart';
-import 'package:food_delivery_app/provider/auth_provider.dart';
+import 'package:food_delivery_app/presentation/screens/login_page_2.dart';
+import 'package:food_delivery_app/utils/my_shared_pref.dart';
 
+Widget defaultHome = LoginPage2();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await MySharedPreference.init();
+
+  bool isLoggedIn = MySharedPreference.userExists();
+  log(isLoggedIn.toString());
+  if (isLoggedIn) {
+    defaultHome = const HomePage();
+  }
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const SplashScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: defaultHome);
   }
 }
 
-class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({Key? key}) : super(key: key);
+// class AuthenticationWrapper extends StatelessWidget {
+//   const AuthenticationWrapper({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final Future<FirebaseApp> _init = Firebase.initializeApp();
-    return FutureBuilder(
-      future: _init,
-      builder: ((context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: Text("Something went wrong"));
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const AuthChecker();
-        }
-        return const Scaffold(
-          body: Center(child: CircularProgressIndicator()),
-        );
-      }),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     final Future<FirebaseApp> _init = Firebase.initializeApp();
+//     return FutureBuilder(
+//       future: _init,
+//       builder: ((context, snapshot) {
+//         if (snapshot.hasError) {
+//           return const Center(child: Text("Something went wrong"));
+//         }
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           return const AuthChecker();
+//         }
+//         return const Scaffold(
+//           body: Center(child: CircularProgressIndicator()),
+//         );
+//       }),
+//     );
+//   }
+// }
 
-class AuthChecker extends ConsumerWidget {
-  const AuthChecker({Key? key}) : super(key: key);
+// class AuthChecker extends ConsumerWidget {
+//   const AuthChecker({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(authStateProvider);
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final state = ref.watch(authStateProvider);
 
-    return state.when(
-        data: ((data) {
-          if (data != null) {
-            return HomePage();
-          } else {
-            return LoginPage();
-          }
-        }),
-        error: (e, s) {
-          return const Center(child: Text("Some error"));
-        },
-        loading: () => const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            ));
-  }
-}
+//     return state.when(
+//         data: ((data) {
+//           if (data != null) {
+//             return const HomePage();
+//           } else {
+//             return LoginPage();
+//           }
+//         }),
+//         error: (e, s) {
+//           return const Center(child: Text("Some error"));
+//         },
+//         loading: () => const Scaffold(
+//               body: Center(child: CircularProgressIndicator()),
+//             ));
+//   }
+// }
